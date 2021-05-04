f39b158d4ec12e2a7c2ba2364d9a592e39d92dba
Kiwi Bui <kiwibui4@gmail.com>
2021-05-04 13:52:00 -0700

    Implemented bowling game

--- src/main/java/com/galvanize/bowling/Game.java
+++ src/main/java/com/galvanize/bowling/Game.java
@@ -1,4 +1,53 @@
 package com.galvanize.bowling;
 
+import java.util.Arrays;
+
 public class Game {
+    int[] scores;
+    int index;
+
+    public Game() {
+        this.scores = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        this.index = 0;
+    }
+
+    public int getScore() {
+        int total = 0;
+
+        for (int frame = 0; frame < 10; frame++) {
+            int scoreOne = scores[(frame * 2)];
+            int scoreTwo = scores[(frame * 2) + 1];
+
+            if (scoreOne == 10) {
+                if (scores[(frame * 2) + 2] == 10) {
+                    if (frame == 9) {
+                        total += 20 + scoreTwo;
+                    } else {
+                        total += 20 + scores[(frame * 2) + 4];
+                    }
+                } else {
+                    total += 10 + scores[(frame * 2) + 2] + scores[(frame * 2) + 3];
+                }
+            } else if (scoreOne + scoreTwo == 10) {
+                total += 10 + scores[(frame * 2) + 2];
+            } else {
+                total += scoreOne + scoreTwo;
+            }
+        }
+
+        return total;
+    }
+
+    public void score(int score) {
+        scores[index] += score;
+        if (score == 10) {
+            if (index > 17)  {
+                index++;
+            } else {
+                index += 2;
+            }
+        } else {
+            index++;
+        }
+    }
 }
--- src/test/java/com/galvanize/bowling/GameTest.java
+++ src/test/java/com/galvanize/bowling/GameTest.java
@@ -1,5 +1,61 @@
 package com.galvanize.bowling;
 
-public class GameTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GameTest {
+    @Test
+    public void startGame() {
+        Game game = new Game();
+    }
+
+    @Test
+    public void gameGetScore() {
+        Game game = new Game();
+
+        assertEquals(0, game.getScore(), "New game score should be 0");
+    }
+
+    @Test
+    public void gameAddScore() {
+        Game game = new Game();
+
+        game.score(1);
+
+        assertEquals(1, game.getScore(), "Game score should be 1");
+    }
+
+    @Test
+    public void gameAddSpare() {
+        Game game = new Game();
+
+        game.score(5);
+        game.score(5);
+        game.score(5);
+
+        assertEquals(20, game.getScore(), "Game score with spare should be 20");
+    }
+
+    @Test
+    public void gameAddStrikes() {
+        Game game = new Game();
+
+        game.score(10);
+        game.score(4);
+        game.score(4);
+
+        assertEquals(26, game.getScore(), "Game score with strike should be 26");
+    }
+
+    @Test
+    public void gameAllStrikes() {
+        Game game = new Game();
+
+        for (int i = 0; i < 12; i++) {
+            game.score(10);
+        }
+
+        assertEquals(300, game.getScore(), "Game score with all strikes should be 300");
+    }
 }
