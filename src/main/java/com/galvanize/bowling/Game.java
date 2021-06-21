d425040981305c4fd1976ff38defeb796e29f7f9
Kiwi Bui <kiwibui4@gmail.com>
2021-06-21 01:07:15 -0700

    cleaned up code

--- src/main/java/com/galvanize/bowling/Game.java
+++ src/main/java/com/galvanize/bowling/Game.java
@@ -13,21 +13,21 @@ public class Game {
         int total = 0;
 
         for (int frame = 0; frame < 10; frame++) {
-            int scoreOne = scores[(frame * 2)];
-            int scoreTwo = scores[(frame * 2) + 1];
+            int scoreOne = this.scores[(frame * 2)];
+            int scoreTwo = this.scores[(frame * 2) + 1];
 
             if (scoreOne == 10) {
-                if (scores[(frame * 2) + 2] == 10) {
+                if (this.scores[(frame * 2) + 2] == 10) {
                     if (frame == 9) {
                         total += 20 + scoreTwo;
                     } else {
-                        total += 20 + scores[(frame * 2) + 4];
+                        total += 20 + this.scores[(frame * 2) + 4];
                     }
                 } else {
-                    total += 10 + scores[(frame * 2) + 2] + scores[(frame * 2) + 3];
+                    total += 10 + this.scores[(frame * 2) + 2] + this.scores[(frame * 2) + 3];
                 }
             } else if (scoreOne + scoreTwo == 10) {
-                total += 10 + scores[(frame * 2) + 2];
+                total += 10 + this.scores[(frame * 2) + 2];
             } else {
                 total += scoreOne + scoreTwo;
             }
@@ -37,15 +37,33 @@ public class Game {
     }
 
     public void score(int score) {
-        scores[index] += score;
-        if (score == 10) {
-            if (index > 17)  {
-                index++;
-            } else {
-                index += 2;
-            }
+        this.scores[this.index] += score;
+
+        if (score == 10 && this.index <= 17)  {
+            this.index++;
+        }
+
+        this.index ++;
+    }
+
+    public int getFrame() {
+        int frame = (int) Math.ceil((this.index + 1) / 2.0);
+
+        return Math.min(frame, 10);
+    }
+
+    public int getRoll() {
+        int roll = this.index % 2 == 0 ? 1 : 2;
+        return index == 20 ? 3 : roll;
+    }
+
+    public boolean getGameOverStatus() {
+        if (index > 20) {
+            return true;
+        } else if (index == 20) {
+            return this.scores[18] != 10;
         } else {
-            index++;
+            return false;
         }
     }
 }
--- src/test/java/com/galvanize/bowling/GameTest.java
+++ src/test/java/com/galvanize/bowling/GameTest.java
@@ -27,6 +27,54 @@ public class GameTest {
     }
 
     @Test
+    public void gameGetFrameTest() {
+        Game game = new Game();
+
+        game.score(1);
+
+        assertEquals(1, game.getFrame(), "Game frame should be 1");
+
+        game.score(1);
+
+        assertEquals(2, game.getFrame(), "Game frame should be 2");
+
+        for (int i = 0; i < 16; i++) {
+            game.score(1);
+        }
+
+        assertEquals(10, game.getFrame(), "Game frame should be 10");
+
+        game.score(1);
+        game.score(1);
+
+        assertEquals(10, game.getFrame(), "Game frame should be 10");
+    }
+
+    @Test
+    public void gameGetRollTest() {
+        Game game = new Game();
+
+        assertEquals(1, game.getRoll(), "Current roll should be 1");
+
+        game.score(1);
+
+        assertEquals(2, game.getRoll(), "Current roll should be 2");
+
+        game.score(1);
+
+        assertEquals(1, game.getRoll(), "Current roll should be 1");
+
+        for (int i = 0; i < 16; i++) {
+            game.score(1);
+        }
+
+        game.score(1);
+        game.score(1);
+
+        assertEquals(3, game.getRoll(), "Current roll should be 3");
+    }
+
+    @Test
     public void gameAddSpare() {
         Game game = new Game();
 
@@ -57,5 +105,35 @@ public class GameTest {
         }
 
         assertEquals(300, game.getScore(), "Game score with all strikes should be 300");
+    }
+
+    @Test
+    public void gameOverTest() {
+        Game game = new Game();
+
+        for (int i = 0; i < 19; i++) {
+            game.score(1);
+        }
+
+        assertEquals(false, game.getGameOverStatus(), "Game score should not be over ");
+
+        game.score(1);
+
+        assertEquals(true, game.getGameOverStatus(), "Game score should be over");
+    }
+
+    @Test
+    public void gameOverAllStrikesTest() {
+        Game game = new Game();
+
+        for (int i = 0; i < 11; i++) {
+            game.score(10);
+        }
+
+        assertEquals(false, game.getGameOverStatus(), "Game score should not be over with bonus last frame");
+
+        game.score(10);
+
+        assertEquals(true, game.getGameOverStatus(), "Game score should be over");
     }
 }
